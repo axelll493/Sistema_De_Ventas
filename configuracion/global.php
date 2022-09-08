1a62bbd14cee3db123a4273e3077ce9c599cde72
Axel Beltrán <beltran.axel.493@gmail.com>
2022-09-08 19:31:23 +0200

    variables de configuración

--- configuracion/global.php
+++ configuracion/global.php
@@ -4,4 +4,12 @@ define("DB_HOST", "localhost");
 
 define("DB_NAME" , "dbsistema");
 
+define("DB_USERNAME" , "root");
+
+define("DB_PASSWORD" , "")
+
+define("DB_ENCODE" , "uft8")
+
+define("SISTEMA_NOMBRE","POS_SYSTEM")
+
 ?>
